d47889ae0073af102f70225de9016d076ac56daa
boudenjal-mohcine <mohcineboudenjal@gmail.com>
2024-02-14 18:13:44 +0100

    add logic of explain query and return result

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from database.oracle_connection import create_connection,close_connection
 import oracledb
 
@@ -9,7 +10,7 @@ from model import generate_response_gpt,generate_response_llama
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -29,10 +30,10 @@ def chatbot_llama():
     return jsonify({'chatbot_response': chatbot_response})
 
 
-@app.route('/results', methods=['GET'])
+@app.route('/results', methods=['POST'])
 def query_results():
     query_param = request.json.get('query')
-
+    print(query_param)
     if not query_param:
         return jsonify({'error': 'Query parameter is required'}), 400
 
@@ -40,14 +41,43 @@ def query_results():
 
     if connection:
         try:
+            # print(data)
             cursor = connection.cursor()
             cursor.execute(query_param)
-            results = cursor.fetchall()
+            data = cursor.fetchall()
+            if data:
 
-            if results:
-                serialized_results = [dict(zip([desc[0] for desc in cursor.description], row)) for row in results]
+                # Get column names
+                column_names = [desc[0] for desc in cursor.description]
+                # Assuming the query returns a list of tuples
 
-                return jsonify(serialized_results)
+                # Generate execution plan
+                cursor.execute("EXPLAIN PLAN FOR " + query_param)
+
+                # Get the execution plan
+                cursor.execute("SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY)")
+                
+                execution_plan = cursor.fetchall()
+                
+                # Format execution plan
+                formatted_execution_plan = []
+                for row in execution_plan[5:]:
+                    if '-' in row[0]:
+                        # If the row contains only dashes, append it as a single entry
+                        continue
+                    else:
+                        row_values = [value.strip() for value in row[0].split("|")]
+                        formatted_execution_plan.append({
+                            'Id': row_values[1],
+                            'Operation': row_values[2],
+                            'Name': row_values[3],
+                            'Rows': row_values[4],
+                            'Bytes': row_values[5],
+                            'Cost': row_values[6],
+                            'Time': row_values[7]
+                        })
+
+                return jsonify({'result':data , 'columns': column_names, 'execution_plan': formatted_execution_plan})
             else:
                 return jsonify({'message': 'No results found for the given query'}), 404
         except oracledb.Error as error:
@@ -55,7 +85,6 @@ def query_results():
             return jsonify({'error': 'Failed to retrieve results'}), 500
         finally:
             close_connection(connection, cursor)
-
 
 # ##pull llama2 model
 # base_model = "frikh-said/query_optimizer_model"
